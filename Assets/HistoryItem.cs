8585304c0cf173f11812306b8c3f6a0238aae3a3
Hendrik Demmer <hendrik.demmer@gmail.com>
2019-01-25 23:22:26 +0100

    Fading history items

---
+++ Assets/HistoryItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HistoryItem : MonoBehaviour
+{
+    public int startTimeline = 9;
+    public int endTimeline = 9;
+    private MeshRenderer mr;
+    
+    private void OnEnable()
+    {
+        mr = GetComponent<MeshRenderer>();
+        TheGame.Instance.items.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        TheGame.Instance.items.Remove(this);
+    }
+
+    private float previousTimeline = -1;
+    void Update()
+    {
+        var timeline = TheGame.Instance.timeline;
+        if (timeline != previousTimeline)
+        {
+            previousTimeline = timeline;
+            var opacity = TheGame.Instance.CurrentFade(startTimeline, endTimeline);
+            
+            mr.material.color = new Color(1f,1f,1f,opacity);
+            
+        }
+    }
+}
---
+++ Assets/TheGame.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheGame : MonoBehaviour
+{
+    public Slider slider;
+
+    public float timeline;
+    public const int LIVES = 9;
+
+    private static TheGame _instance;
+    public static TheGame Instance
+    {
+        get { return _instance; }
+    }
+    public List<HistoryItem> items;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void OnDisable()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
+    }
+
+    void Update()
+    {
+        timeline = 1 + Mathf.Clamp01(slider.value) * (LIVES - 1);
+    }
+
+    public float CurrentFade(int startTimeline, int endTimeline)
+    {
+        var opacity = 1f;
+        if (timeline <= startTimeline)
+        {
+            opacity = 1f - Mathf.Clamp01(startTimeline - timeline);
+        } else if (timeline >= endTimeline)
+        {
+            opacity = 1f - Mathf.Clamp01(timeline - endTimeline);
+        }
+
+        return opacity;
+    }
+}
