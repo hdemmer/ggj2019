9100e41696b2e1971ab5b046e010f86ba2b2dbd8
juna8001 <juna8001@gmail.com>
2019-01-26 11:57:16 +0100

    fix

--- Assets/HistoryItem.cs
+++ Assets/HistoryItem.cs
@@ -5,46 +5,46 @@ using UnityEngine;
 
 public class HistoryItem : MonoBehaviour
 {
-    public int startTimeline = 9;
-    public int endTimeline = 9;
-    private GlitchController gc;
+	public int startTimeline = 9;
+	public int endTimeline = 9;
+	private GlitchController gc;
 
-    [SerializeField]
-    private float opacity = 0f;
-    
-    private void OnEnable()
-    {
-        gc = GetComponent<GlitchController>();
-        TheGame.Instance.items.Add(this);
-    }
+	[SerializeField]
+	private float opacity = 0f;
 
-    private void OnDisable()
-    {
-        var theGame = TheGame.Instance;
-        if (theGame)
-        {
-            theGame.items.Remove(this);
-        }
-    }
+	private void OnEnable()
+	{
+		gc = GetComponent<GlitchController>();
+		TheGame.Instance.items.Add(this);
+	}
 
-    private float previousTimeline = -1;
-    void Update()
-    {
-        var timeline = TheGame.Instance.timeline;
-        if (timeline != previousTimeline && gc != null)
-        {
-            previousTimeline = timeline;
-            var opacity = TheGame.Instance.CurrentFade(startTimeline, endTimeline);
+	private void OnDisable()
+	{
+		var theGame = TheGame.Instance;
+		if (theGame)
+		{
+			theGame.items.Remove(this);
+		}
+	}
+
+	private float previousTimeline = -1;
+	void Update()
+	{
+		var timeline = TheGame.Instance.timeline;
+		if (timeline != previousTimeline && gc != null)
+		{
+			previousTimeline = timeline;
+			var opacity = TheGame.Instance.CurrentFade(startTimeline, endTimeline);
 
 			gc.disapear = 1f - opacity;
 
 			this.opacity = opacity;
 		}
-        }
-    }
 
-    public float GetOpacity()
-    {
-        return opacity;
-    }
+	}
+
+	public float GetOpacity()
+	{
+		return opacity;
+	}
 }
