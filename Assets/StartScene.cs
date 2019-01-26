2fce596ca73902808bff83ff3bde86e23695e7fd
Hendrik Demmer <hendrik.demmer@gmail.com>
2019-01-26 11:54:40 +0100

    Start scene with play button

---
+++ Assets/StartScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScene : MonoBehaviour
+{
+    public Button button;
+
+    void Awake()
+    {
+        button.onClick.AddListener(() => { SceneManager.LoadScene("Game", LoadSceneMode.Single); });
+    }
+}
