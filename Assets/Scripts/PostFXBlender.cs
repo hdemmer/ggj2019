caf20b3784f67a5379285b1a2ce296ec9509b5ea
juna8001 <juna8001@gmail.com>
2019-01-26 15:48:26 +0100

    x

---
+++ Assets/Scripts/PostFXBlender.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class PostFXBlender : MonoBehaviour
+{
+	private void Update()
+	{
+		PostProcessingProfile p = new PostProcessingProfile();
+		PostProcessingContext c = new PostProcessingContext();
+		PostProcessingComponentBase com;
+		postpr
+	}
+}
